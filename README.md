7be34ccb8cfec69ab724830b6853518675112015
Patrick Tsui <patrick.tsui@wunderkind.co>
2022-02-28 13:50:29 -0500

    Updated Empty It Function to allow values to be completely empty

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
-# Golang Mirror Extension
+# Golang HTML Mirror Extension
 
-An extension to help make it easier to mirror Golang variables right away.
+This extensions focuses on mirroring Golang HTML Variables right away.
+
+## Features
+
+- Converts Golang HTML values into Mirror Variables
+- Empty Golang HTML Variables
+- Copy tex from inside textarea box.
+
+## Uploading Extensions
+
+- git clone the repo
+- Go to your Manage Extensions
+- Load unpacked the folder you git cloned into your computer
 
 ## For Example
 
 Say that you have `{{ $TestExample := "Here is some text." }}`. When you copy and paste your code into the text area box, you can hit the button to generate `{{ $TestExample := $.TestExample }}`. This works for more than one at a time.
-
-## Uploading Extensions
-
-Go to your Manage Extensions and Load unpacked the folder of where the code is. You should be all set to test it out.
--- popup/popup.js
+++ popup/popup.js
@@ -1,32 +1,38 @@
-const btnTry = document.getElementById('try-btn');
-const btnEmpty = document.getElementById('empty-btn');
-const btnCopy = document.getElementById('copy-btn');
+const btnTry = document.getElementById("try-btn");
+const btnEmpty = document.getElementById("empty-btn");
+const btnCopy = document.getElementById("copy-btn");
 
 const tryHandler = (makeEmpty = false) => {
-	const text = document.getElementById('textarea-copy');
+	const text = document.getElementById("textarea-copy");
 	text.value = text.value
-		.split('\n')
+		.split("\n")
 		.map((element) => {
 			let finalSentence = element;
+			if (makeEmpty === true) {
+				const sentence = finalSentence.split(" ").filter((word) => word !== "");
+				sentence[3] = '""';
+				finalSentence = sentence.join(" ");
+				return finalSentence;
+			}
 			if (finalSentence.includes(':= "')) {
 				const sentence = finalSentence.split(' "');
-				const array = `${sentence[0]} "" }}`.trim().split(' ');
-				makeEmpty === true ? true : (array[array.indexOf('""')] = `$.${array[1].slice(1)}`);
-				finalSentence = array.join(' ');
+				const sentArr = `${sentence[0]} "" }}`.trim().split(" ");
+				sentArr[sentArr.indexOf('""')] = `$.${sentArr[1].slice(1)}`;
+				finalSentence = sentArr.join(" ");
 			}
 			return finalSentence;
 		})
-		.join('\n');
+		.join("\n");
 };
 
 const copyHandler = () => {
-	const copyText = document.getElementById('textarea-copy');
+	const copyText = document.getElementById("textarea-copy");
 	navigator.clipboard.writeText(copyText.value).then(
-		() => alert('Copied the text'),
-		(err) => alert('Could not copy', err)
+		() => alert("Copied the text"),
+		(err) => alert("Could not copy", err)
 	);
 };
 
-btnTry.addEventListener('click', tryHandler);
-btnEmpty.addEventListener('click', tryHandler.bind(null, true));
-btnCopy.addEventListener('click', copyHandler);
+btnTry.addEventListener("click", tryHandler);
+btnEmpty.addEventListener("click", tryHandler.bind(null, true));
+btnCopy.addEventListener("click", copyHandler);
